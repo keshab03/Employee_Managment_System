7a491a8727f95303571a49b6ab2e362d36f284c4
Keshab Mahanta <94249491+keshab03@users.noreply.github.com>
2023-09-17 12:48:58 +0530

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,5 +1,4 @@
 const PORT = 5500
-// const MONGODB_URI = 'mongodb+srv://keshabdb:o2iI5DdxW9eGjCZn@cluster0.1xxdkct.mongodb.net/employee?retryWrites=true&w=majority'
 const MONGODB_URI = 'mongodb://localhost:27017/EmployeeManagement'
 
 module.exports = { PORT, MONGODB_URI }
