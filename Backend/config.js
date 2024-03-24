4d623e6a66aba7400a8ea8ce1dc826015d124a5f
unknown <keshabmahanta03@gmail.com>
2024-03-24 17:22:16 +0530

    Update Libries

--- Backend/config.js
+++ Backend/config.js
@@ -1,4 +1,4 @@
-const PORT = 5500
-const MONGODB_URI = 'mongodb://localhost:27017/EmployeeManagement'
+const PORT = 5500;
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/EmployeeManagement'; // Use IPv4 localhost
 
-module.exports = { PORT, MONGODB_URI }
+module.exports = { PORT, MONGODB_URI };
--- Backend/index.js
+++ Backend/index.js
@@ -14,15 +14,17 @@ app.use(cors());
 
 // Connect to MongoDB database
 mongoose.set('strictQuery', true)
-
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch(err => console.error(err));
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
 
 // Routes
 app.use('/emp', employeeRoutes);
